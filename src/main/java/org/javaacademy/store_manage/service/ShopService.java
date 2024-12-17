cebfce8a5403754b33c8bf89992310ead828c876
AaronDeluna <aaron_deluna@mail.ru>
2024-12-17 18:31:08 +0300

    Рефакторинг: улучшил структуру проекта, вынес логику в методы extractShopInfo и convertToJson

--- src/main/java/org/javaacademy/store_manage/service/ShopService.java
+++ src/main/java/org/javaacademy/store_manage/service/ShopService.java
@@ -1,5 +1,6 @@
 package org.javaacademy.store_manage.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jayway.jsonpath.JsonPath;
 import lombok.extern.slf4j.Slf4j;
@@ -15,46 +16,49 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Service
-@Slf4j
 public class ShopService {
     private static final String INVALID_RESPONSE_ERROR = "Ошибка: Запрос не выполнен успешно или тело ответа пустое.";
     @Value("${shop.first.api}")
     private String firstUrl;
     @Value("${shop.second.api}")
     private String secondUrl;
+    private final List<String> responseStatusList = new ArrayList<>();
     private final OkHttpClient client = new OkHttpClient();
     private final ObjectMapper objectMapper = new ObjectMapper();
 
     public List<String> getShopInfo() throws IOException {
-        List<String> responseStatusList = new ArrayList<>();
-        for (String api : List.of(firstUrl, secondUrl)) {
-            Request request = buildRequest(api);
+        for (String url : List.of(firstUrl, secondUrl)) {
+            Request request = buildRequest(url);
 
             try (Response response = client.newCall(request).execute()) {
                 if (!response.isSuccessful() || response.body() == null) {
                     throw new RuntimeException(INVALID_RESPONSE_ERROR);
                 }
-                responseStatusList.add(extractAndConvertShopInfo(response));
+                String responseBody = response.body().string();
+                ShopInfoDto shopInfoDto = extractShopInfo(responseBody);
+                responseStatusList.add(convertToJson(shopInfoDto));
             }
         }
 
         return responseStatusList;
     }
 
-    private Request buildRequest(String api) {
+    private Request buildRequest(String url) {
         return new Request.Builder()
-                .url(api)
+                .url(url)
                 .get()
                 .build();
     }
 
-    private String extractAndConvertShopInfo(Response response) throws IOException {
-        String responseBody = response.body().string();
+    private ShopInfoDto extractShopInfo(String responseBody) {
         String name = JsonPath.parse(responseBody).read("$.name", String.class);
         String shopStatus = JsonPath.parse(responseBody).read("$.shopStatus", String.class);
         String timeOpen = JsonPath.parse(responseBody).read("$.time_open", String.class);
         String timeClose = JsonPath.parse(responseBody).read("$.time_close", String.class);
-        ShopInfoDto shopInfoDto = new ShopInfoDto(name, shopStatus, timeOpen, timeClose);
+        return new ShopInfoDto(name, shopStatus, timeOpen, timeClose);
+    }
+
+    private String convertToJson(ShopInfoDto shopInfoDto) throws JsonProcessingException {
         return objectMapper.writeValueAsString(shopInfoDto);
     }
 }
